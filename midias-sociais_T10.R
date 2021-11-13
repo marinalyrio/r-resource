bc8d43e6e94f6037c65b6da1f31b62f802b3d7f4
Marina Lyrio <89712375+marinalyrio@users.noreply.github.com>
2021-11-13 00:58:17 -0300

    Update midias-sociais_T10.R
    Adicionados dois pacotes (stringr e tidytext) e adicionadas as partes de análise de sentimento e plot da análise por ano.

--- midias-sociais_T10.R
+++ midias-sociais_T10.R
@@ -5,7 +5,7 @@
 # Caio Ramos        - Matrícula: A58338177
 # Isabela Massaro   - Matrícula: A58337886
 # Márcia Magalhães  - Matrícula: A58331043
-# Marina 			      - Matrícula:
+# Marina Lyrio      - Matrícula: A55310554
 #
 # Turma: T10
 # Curso: MBA EM BUSINESS ANALYTICS E BIG DATA
@@ -20,6 +20,10 @@ install.packages("lubridate")
 library(lubridate)
 install.packages("dplyr")
 library(dplyr)
+install.packages("stringr")
+library(stringr)
+install.packages("tidytext")
+library(tidytext)
 
 # :::Limpa os dados da memória
 rm(list = ls())
@@ -56,6 +60,46 @@ View(twitters)
 # 3) (Aprendiz.N.Superv.) - Análise de Sentimento.
 # (Isabela)Analise-1: geral
 # (Marina) Analise-2: por ano (2014 - 2021)
+
+twitters_2 <- twitters
+nrow(twitters_2)
+
+#atribuindo uma identificação para cada tweet
+for (i in 1:nrow(twitters_2)) {
+  twitters_2$row[i] <- i
+}
+
+#abrindo cada tweet por palavra e definindo um sentimento (dicionário bing)
+twitters_word <- twitters_2 
+twitters_word <- unnest_tokens(twitters_word, input = tweet, output = word) 
+twitters_word <-twitters_word %>% inner_join(get_sentiments("bing"), by = c("word" = "word"))
+
+#removendo as colunas que não vão ser usadas
+twitters_word <- twitters_word %>% select (-target, -insult)
+
+#abrindo os tweets por sentimento, e agrupando por ano
+twitters_ano <- twitters_word %>% count(row, sentiment,year) %>% spread(key = sentiment, value = n)
+twitters_word <- twitters_word %>% count(row, sentiment) %>% spread(key = sentiment, value = n)
+
+#trocando NA por 0, para cálculo do sentimento líquido
+twitters_word$negative[which(is.na(twitters_word$negative))] <- 0
+twitters_word$positive[which(is.na(twitters_word$positive))] <- 0
+
+twitters_ano$negative[which(is.na(twitters_ano$negative))] <- 0
+twitters_ano$positive[which(is.na(twitters_ano$positive))] <- 0
+
+#calculando o sentimento líquido
+twitters_word$sentimento <- twitters_word$positive - twitters_word$negative
+twitters_word <- twitters_word %>% select (-positive, -negative)
+
+twitters_ano$sentimento <- twitters_ano$positive - twitters_ano$negative
+twitters_ano <- twitters_ano %>% select (-positive, -negative)
+
+#plotando por anos
+twitters_ano %>% ggplot(aes(x = nrow(twitters_ano), y = sentimento, color = row)) +geom_col() +
+  facet_wrap(~year)
+
+twitters_ano %>% ggplot(aes(x = year, y = sentimento, color = row)) +geom_col()
 
 
 # (Marcia)
